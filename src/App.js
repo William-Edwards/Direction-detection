9714e71d0971eb80bd8f90974096597283a111d6
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2023-04-06 17:51:44 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
 import "./App.css";
-import { nextFrame } from "@tensorflow/tfjs";
 // 2. TODO - Import drawing utility here
-import {drawRect} from "./utilities"; 
+import { drawRect } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
@@ -14,8 +13,8 @@ function App() {
   // Main function
   const runCoco = async () => {
     // 3. TODO - Load network 
-    const net = await tf.loadGraphModel('https://livelong.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json')
-    
+    const net = await tf.loadGraphModel('https://tfoddirections.s3.eu-gb.cloud-object-storage.appdomain.cloud/model.json')
+
     // Loop and detect hands
     setInterval(() => {
       detect(net);
@@ -44,21 +43,21 @@ function App() {
 
       // 4. TODO - Make Detections
       const img = tf.browser.fromPixels(video)
-      const resized = tf.image.resizeBilinear(img, [640,480])
+      const resized = tf.image.resizeBilinear(img, [640, 480])
       const casted = resized.cast('int32')
       const expanded = casted.expandDims(0)
       const obj = await net.executeAsync(expanded)
-      
-      const boxes = await obj[4].array()
-      const classes = await obj[5].array()
+
+      const boxes = await obj[3].array()
+      const classes = await obj[0].array()
       const scores = await obj[6].array()
-    
+
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)  
-      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.9, videoWidth, videoHeight, ctx)}); 
+      requestAnimationFrame(() => { drawRect(boxes[0], classes[0], scores[0], 0.9, videoWidth, videoHeight, ctx) });
 
       tf.dispose(img)
       tf.dispose(resized)
@@ -69,14 +68,14 @@ function App() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(() => { runCoco() });
 
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
           style={{
             position: "absolute",
             marginLeft: "auto",
